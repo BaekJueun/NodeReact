adb13ff731e79f705e1fdfba30ab2b30cb06d0ea
BAEK STUDIO <sosaer88@naver.com>
2018-11-02 05:22:03 +0900

    react : state

--- part.3/src/Container.js
+++ part.3/src/Container.js
@@ -1,12 +1,31 @@
 import React, {Component,Fragment} from 'react';
 
 class Container extends Component{
+    state = {
+        num : 0
+    };
+    btnPlus = () => {
+        this.setState({
+            num : this.state.num +1
+        })
+    }
+    btnMius = () =>{
+        this.setState({
+            num : this.state.num -1
+        })
+    }
     render(){
         return(
             <Fragment>
                 <div id="container">
                     <div className="inner">
-                        MainVisual
+                        <div className="mainVisual">MainVisual</div>
+                        <div className="count">
+                            <h2>카운터</h2>
+                            <p>값 : {this.state.num} </p>
+                            <button onClick={this.btnPlus}>+</button>
+                            <button onClick={this.btnMius}>-</button>
+                        </div>
                     </div>
                 </div>
             </Fragment>
